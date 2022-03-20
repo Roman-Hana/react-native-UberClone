c99d93a9f9ec0e059c8d517693c5d14587211d72
Roman <romanhanaua@gmail.com>
2022-03-20 20:04:20 +0200

    setting up redux and bulding home screen

---
+++ screens/Home.js
@@ -0,0 +1,27 @@
+import { View, Text, SafeAreaView, StyleSheet, Image } from "react-native";
+import React from "react";
+import tw from "twrnc";
+
+const Home = () => {
+  return (
+    <SafeAreaView style={[styles.droidSafeArea, tw`bg-white h-full`]}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{ width: 100, height: 100, resizeMode: "contain" }}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  droidSafeArea: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? 25 : 0,
+  },
+});
+
+export default Home;

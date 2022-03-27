8108f684f4f6b212b023094138bf951e2d72d4a5
Roman <romanhanaua@gmail.com>
2022-03-27 23:28:31 +0300

    google autocomplete add

--- componets/NavOptions.js
+++ componets/NavOptions.js
@@ -9,7 +9,7 @@ import {
 import React from "react";
 import tw from "twrnc";
 import { Icon } from "react-native-elements";
-import { useNavigation } from "@react-navigation/native"
+import { useNavigation } from "@react-navigation/native";
 
 const data = [
   {
@@ -37,7 +37,10 @@ export default function NavOptions() {
       keyExtractor={(item) => item.id}
       horizontal
       renderItem={({ item }) => (
-        <TouchableOpacity onPress={() => navigation.navigate(item.screen)} style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate(item.screen)}
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+        >
           <View>
             <Image style={styles.image} source={{ uri: item.image }} />
             <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
--- screens/Home.js
+++ screens/Home.js
@@ -2,8 +2,12 @@ import { View, Text, SafeAreaView, StyleSheet, Image } from "react-native";
 import React from "react";
 import tw from "twrnc";
 import NavOptions from "../componets/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_API_KEY } from "@env";
 
 const Home = () => {
+
+  
   return (
     <SafeAreaView style={[styles.droidSafeArea, tw`bg-white h-full`]}>
       <View style={tw`p-5`}>
@@ -11,6 +15,20 @@ const Home = () => {
           style={{ width: 100, height: 100, resizeMode: "contain" }}
           source={{
             uri: "https://links.papareact.com/gzs",
+          }}
+        />
+        <GooglePlacesAutocomplete
+          styles={{ container: { flex: 0 }, textInput: { fontSize: 18 } }}
+          placeholder="Search"
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+          minLength={2}
+          enablePoweredByContainer={false}
+          fetchDetails={true}
+          query={{ key: GOOGLE_API_KEY, language: "en" }}
+          onPress={(data, details = null) => {
+            console.log(data)
+            console.log(details)
           }}
         />
         <NavOptions />
